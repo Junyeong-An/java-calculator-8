49d153b9b0b487f728d9e1913111c23b4d61820d
Junyeong-An <dev.jun0321@gmail.com>
2025-10-20 06:11:13 +0900

    feat(view): 사용자 입력, 출력 기능 구현

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,10 @@
+package calculator.view;
+
+public class OutputView {
+
+    private static final String RESULT_MESSAGE = "결과 : ";
+
+    public void printResult(int sum) {
+        System.out.println(RESULT_MESSAGE + sum);
+    }
+}
